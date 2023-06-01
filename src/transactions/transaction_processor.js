6fee8abe951fa928f4c3f36deafa9710e8f48730
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-06-01 19:22:54 +0100

    reformats transaction process

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,52 +1,33 @@
-var txr = [];
+const processTransactions = (transActions) => {
 
-function processTransactions(transActions) {
-
-    txr = [];
-
-    if(!validateTransactions(transActions)) {
+    if(transActions === undefined) {
         throw new Error("Undefined collection of transactions")
     }
 
-    let txCount = {}
-
-    const numberOfTransactions = transActions.length;
-
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transActions[i];
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    }
+    let txCount = transActions.reduce((count, transAction) => {
+        count[transAction] ? count[transAction]++ : count[transAction] = 1;
+        return count;
+    }, {});
 
     txCount = sortByAmountThenName(txCount);
     
     // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
-    });
+    const txr = Object.keys(txCount).map(key => `${key} ${txCount[key]}`);
 
     return txr;
 }
 
-function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
+const sortByAmountThenName = (txCount) => {
+    let sortedKeys = Object.keys(txCount).sort((itemOne, itemTwo) => {
+        return txCount[itemTwo] - txCount[itemOne] || itemOne.localeCompare(itemTwo);
+    });
 
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
+    const sortedResults = sortedKeys.reduce((results, key) => {
+        results[key] = txCount[key];
+        return results;
+    }, {});
 
     return sortedResults;
-}
-
-
-function validateTransactions(transactions) {
-    if(transactions === undefined) {
-        return false;
-    } 
-
-    return true;
 }
 
 module.exports = processTransactions;
